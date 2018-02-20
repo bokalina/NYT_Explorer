539b9c508d477dbe546c9704330f324c6e4c2bdb
bokalina <milosevicbojana90@gmail.com>
2018-02-20 12:06:57 -0800

    adding react

--- js/NYT_Explorer.js
+++ js/NYT_Explorer.js
@@ -7,7 +7,17 @@ $.ajax({
   method: 'GET',
 }).done(function(result) {
   // console.log(result);
-  console.log(result.response.docs[0].web_url);
+  var top20 = result.response.docs.slice(0,20);
+  for (var i = 0; i < 20; i++) {
+  	var doc = top20[i];
+  	console.log(doc.web_url);
+  }
+
+	ReactDOM.render(
+	  <h1>Hello, world!</h1>,
+	  document.getElementById('root')
+	);
+
   // console.log(result.response.docs[1].web_url);
 }).fail(function(err) {
   throw err;
