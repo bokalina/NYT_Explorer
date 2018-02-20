468cb3b6c772cf39d0c169cbb15b2b813916ace5
bokalina <milosevicbojana90@gmail.com>
2018-02-20 11:27:09 -0800

    html and basic js

--- js/NYT_Explorer.js
+++ js/NYT_Explorer.js
@@ -1,1 +1,14 @@
-
+var url = "https://api.nytimes.com/svc/archive/v1/2002/1.json";
+url += '?' + $.param({
+  'api-key': "51e7864a4e7c4a2b990dd8e41f131457"
+});
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+  // console.log(result);
+  console.log(result.response.docs[0].web_url);
+  // console.log(result.response.docs[1].web_url);
+}).fail(function(err) {
+  throw err;
+});
